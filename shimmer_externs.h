936358abff75960ab40e1125c1ecaf08ba17fbe8
Mark Nolan <mnolan@shimmersensing.com>
2025-01-28 16:17:57 +0000

    DEV-90 #time 5m commit of more common source files

---
+++ shimmer_externs.h
@@ -0,0 +1,18 @@
+/*
+ * shimmer_externs.h
+ *
+ *  Created on: Sep 23, 2024
+ *      Author: MarkNolan
+ */
+
+#ifndef SHIMMER_EXTERNS_H_
+#define SHIMMER_EXTERNS_H_
+
+#include "shimmer_definitions.h"
+
+#include "../log-and-stream-app/battery.h"
+
+extern STATTypeDef shimmerStatus;
+extern BattStatus batteryStatus;
+
+#endif /* SHIMMER_EXTERNS_H_ */
---
+++ shimmer_globals.h
@@ -0,0 +1,17 @@
+/*
+ * shimmer_globals.h
+ *
+ *  Created on: Sep 23, 2024
+ *      Author: MarkNolan
+ */
+
+#ifndef SHIMMER_GLOBALS_H_
+#define SHIMMER_GLOBALS_H_
+
+#include "shimmer_definitions.h"
+#include "battery.h"
+
+STATTypeDef shimmerStatus;
+BattStatus batteryStatus;
+
+#endif /* SHIMMER_GLOBALS_H_ */
