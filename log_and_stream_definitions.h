7e5a7900f85485b791a63c500fe51e2675cb0048
Mark Nolan <mnolan@shimmersensing.com>
2025-04-01 20:56:14 +0100

    DEV-100 #time 1m usbPluggedIn bool in sensor status struct

--- log_and_stream_definitions.h
+++ log_and_stream_definitions.h
@@ -40,6 +40,8 @@ typedef volatile struct STATTypeDef_t
   uint8_t sdSyncEnabled          : 1;
   uint8_t sdSyncCommTimerRunning : 1;
 
+  uint8_t usbPluggedIn           : 1;
+
   uint8_t toggleLedRedCmd        : 1;
 #if defined(SHIMMER3R)
   uint32_t testResult;
