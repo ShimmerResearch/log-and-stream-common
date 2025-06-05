58f53759a4916d2b01dad220e2d1fbc80af8c823
Mark Nolan <mnolan@shimmersensing.com>
2025-06-05 10:55:43 +0100

    DEV-278 #time 10m update SR board versions that have nBOOT inverted

--- Boards/shimmer_boards.h
+++ Boards/shimmer_boards.h
@@ -109,7 +109,7 @@ uint8_t ShimBrd_isLis3mdlPresent(void);
 uint8_t isAds7028Present(void);
 uint8_t ShimBrd_isI2c4Supported(void);
 uint8_t ShimBrd_isBoardSr48_6_0(void);
-uint8_t ShimBrd_isBoardSr48_7_0(void);
+uint8_t ShimBrd_isI2cOnPPGControlledByAdcChip(void);
 uint8_t ShimBrd_areMcuAdcsUsedForSensing(void);
 
 uint8_t ShimBrd_isBoardSrNumber(uint8_t exp_brd_id, uint8_t exp_brd_major, uint8_t exp_brd_minor);
