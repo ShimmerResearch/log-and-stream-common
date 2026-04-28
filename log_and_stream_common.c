2a667ada8d895dc2a91a021cc7287f2629210874
Mark Nolan <mnolan@shimmersensing.com>
2026-04-28 17:02:40 +0100

    DEV-719 #time 10m Block USB being enabled if in the middle of sensing

--- log_and_stream_common.c
+++ log_and_stream_common.c
@@ -275,11 +275,11 @@ void LogAndStream_dockOrUsbStateUpdate(void)
   uint8_t usbChanged = (prevUsb != shimmerStatus.usbPluggedIn);
 #endif
 
-  if (dockChanged
+  if ((dockChanged
 #if defined(SHIMMER3R)
       || usbChanged
 #endif
-      || shimmerStatus.booting)
+      || shimmerStatus.booting) && LogAndStream_allowProcessDockChange())
   {
     LogAndStream_dockedStateChange();
   }
@@ -406,7 +406,7 @@ void LogAndStream_assignSdToDock(void)
   {
     Board_sd2Pc();
   }
-  if (!shimmerStatus.sensing)
+  if (LogAndStream_allowProcessDockChange())
   {
     DockUart_init();
   }
@@ -615,3 +615,6 @@ void LogAndStream_buildShimmerPrefix(char *outBuf, size_t outBufLen)
   LogAndStream_buildShimmerMacSuffix(suffix, sizeof(suffix));
   snprintf(outBuf, outBufLen, "Shimmer %s", suffix);
 }
+
+bool LogAndStream_allowProcessDockChange(void){
+  /* Don't allow dock change processing if we're in the middle of sensing */  return !shimmerStatus.sensing;}
